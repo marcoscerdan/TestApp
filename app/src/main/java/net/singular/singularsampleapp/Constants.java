623cd7c55425fbc9c414b7efe35e135b10825bca
marcoscerdan <marcoscerdan@develop.mx>
2022-08-08 18:28:02 -0300

    se agrega la app

---
+++ app/src/main/java/net/singular/singularsampleapp/Constants.java
@@ -0,0 +1,10 @@
+package net.singular.singularsampleapp;
+
+public interface Constants {
+    String API_KEY = "<develop_2460caec>";
+    String SECRET = "<aafbe6757917268c1a009fff4324091e>";
+
+    String DEEPLINK_KEY = "deeplink";
+    String PASSTHROUGH_KEY = "passthrough";
+    String IS_DEFERRED_KEY = "isDeferred";
+}
